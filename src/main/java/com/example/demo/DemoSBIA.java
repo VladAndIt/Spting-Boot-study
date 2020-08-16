eb27be314c7386af3a3bc27f69d76fdf6e75a4ab
Vlad <trrunovva@gmail.com>
2020-08-16 16:21:43 +0300

    Reed

---
+++ src/main/java/com/example/demo/DemoSBIA.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = DemoSBIA.class)
+@WebAppConfiguration
+public class DemoSBIA {
+    @Test
+    public void contextLoads() {
+
+    }
+}
