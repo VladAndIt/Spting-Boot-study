15b227cd27d6d8d8880b0ffcb4017f7a27e3eb99
Vlad <trrunovva@gmail.com>
2020-08-21 11:01:54 +0300

    freeCodeCamp Final

--- src/main/java/com/example/demo/dao/PersonalDataAccessInService.java
+++ src/main/java/com/example/demo/dao/PersonalDataAccessInService.java
@@ -1,6 +1,8 @@
 package com.example.demo.dao;
 
 import com.example.demo.module.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,12 @@ import java.util.UUID;
 
 @Repository("postgres")
 public class PersonalDataAccessInService implements PersonDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonalDataAccessInService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public int insertPerson(UUID id, Person person) {
@@ -17,12 +25,24 @@ public class PersonalDataAccessInService implements PersonDao {
 
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(),"From postgres DB"));
+        final String sql = "SELECT id,name FROM person";
+        return jdbcTemplate.query(sql, (resultSet, index) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
+//        return List.of(new Person(UUID.randomUUID(), "From postgres DB"));
     }
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id,name FROM person WHERE id = ?";
+        Person person = jdbcTemplate.queryForObject(sql, new Object[]{id},(resultSet, index) -> {
+            UUID personId = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(personId, name);
+        });
+        return Optional.ofNullable(person);
     }
 
     @Override
